caa54d8ccedbbf4f3b2856e46530f6fbe3835f65
XX <fallwall19@gmail.com>
2019-08-30 15:17:18 -0400

    useEffect

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 https://reactjs.org/docs/hooks-intro.html
+
+"Hooks are functions that let you “hook into” React state and lifecycle features from function components. Hooks don’t work inside classes — they let you use React without classes."
+
+"React provides a few built-in Hooks like useState. You can also create your own Hooks to reuse stateful behavior between different components. We’ll look at the built-in Hooks first."
+
+"Only call Hooks at the top level. Don’t call Hooks inside loops, conditions, or nested functions."
+
--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default function App() {
-  const[count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  // Declare multiple state variables
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
 
-    return (
-      <div>
-        <p>You clicked {count} times</p>
-        <button onClick={()=> setCount(count +1)}>Click Here</button>
-      </div>
-    )
-  
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+      // Update the document title using the browser API
+      document.title = `You clicked ${count} times`;
+    });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Here</button>
+    </div>
+  )
+
 }
 
